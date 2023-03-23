30b4eddcbc89d5b6cf82838f4ddb744fd163f6c0
Daniel F. Retamal <110442700+Wim0@users.noreply.github.com>
2023-03-22 21:58:23 -0300

    caja de comentarios + firebase (#1)
    configuracion de firebase con una caja de comentarios. tambien se le dio un estilo a la caja de comentarios
    
    Co-authored-by: danielreva <83436148+danielreva@users.noreply.github.com>

---
+++ comentarios.js
@@ -0,0 +1,52 @@
+const formulario = document.querySelector('#formulario');
+
+//Para poder guardar los comentarios
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const nombre = document.querySelector('#nombre').value;
+    const comentario = document.querySelector('#comentario').value;
+
+    const data = {
+        nombre: nombre,
+        comentario: comentario
+    };
+
+    fetch('https://chatgpt-ejemplo-web-default-rtdb.firebaseio.com/', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+});
+
+//Configuracion para envia los comentarios a firebase
+function enviarComentario(nombre, comentario) {
+    // Configurar la conexión a Firebase
+    const firebaseConfig = {
+        apiKey: "AIzaSyAlcCuGWisPalWxkiMUdQ-sabaddCOjdXQ",
+        authDomain: "chatgpt-ejemplo-web.firebaseapp.com",
+        projectId: "chatgpt-ejemplo-web",
+        storageBucket: "chatgpt-ejemplo-web.appspot.com",
+        messagingSenderId: "561045361208",
+        appId: "1:561045361208:web:dd48ed0d4587bd4ef40ccd"
+    };
+    firebase.initializeApp(firebaseConfig);
+
+    // Escribir el comentario en la colección "comentarios"
+    const db = firebase.firestore();
+    db.collection("comentarios").add({
+        nombre: nombre,
+        comentario: comentario
+    })
+        .then((docRef) => {
+            console.log("Comentario almacenado con ID: ", docRef.id);
+        })
+        .catch((error) => {
+            console.error("Error al almacenar comentario: ", error);
+        });
+}
